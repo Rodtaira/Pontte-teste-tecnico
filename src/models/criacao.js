a58df1a309117ea21c7c308253f3148f2f112800
Rodtaira <rodtairaunb@gmail.com>
2020-10-01 18:58:57 -0300

    Model of Criacoes

---
+++ src/models/criacao.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+  const Criacoes = sequelize.define('Criacoes', {
+    id:
+      { unique: true, 
+        allowNull: false, 
+        primaryKey: true,
+        type: DataTypes.UUID
+      }, 
+    name: 
+      {
+        type: DataTypes.STRING, 
+        allowNull:false
+      },
+    email: 
+      { 
+        allowNull: false, 
+        unique: true, 
+        type: DataTypes.STRING
+      },      
+    cpf: 
+      {
+        type: DataTypes.STRING, 
+        allowNull: false, 
+      },
+    loanValue: 
+      {
+        type: DataTypes.INTEGER, 
+        allowNull: false
+      },
+    monthlyIncome: 
+      {
+        type: DataTypes.INTEGER
+      },
+    birthDate: 
+      {
+        type: DataTypes.DATE
+      },
+    maritalStatus: 
+      {
+        type: DataTypes.STRING
+      },
+    address: 
+      {
+        type: DataTypes.STRING
+      }
+  }, {});
+  Criacoes.associate = function(models) {
+    // associations can be defined here
+  };
+  return Criacoes;
+};
