90476186131b4ace3b2ea9b7e08cde6133870057
Rodtaira <rodtairaunb@gmail.com>
2020-10-06 18:37:47 -0300

    removendo alguns controllers e rotas

--- src/routes/criacaoRoutes.js
+++
@@ -1,8 +0,0 @@
-const CriacaoController =   require('../controllers/CriacaoController')
-const {Router}          =   require('express')
-
-const router = Router()
-
-router.post('/criacao', CriacaoController.createCriacao)
-
-module.exports = router
